2868754a6068475cf68c7fdf7c710bd0020bac43
Derek Stephen McLean <delboy1978uk@gmail.com>
2016-01-06 04:30:51 +0000

    Added Migrations

--- migrations/Version20160106014455.php
+++ migrations/Version20160106014455.php
@@ -8,7 +8,7 @@ use Doctrine\DBAL\Schema\Schema;
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20160105193435 extends AbstractMigration
+class Version20160106014455 extends AbstractMigration
 {
     /**
      * @param Schema $schema
