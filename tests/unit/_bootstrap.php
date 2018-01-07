c5b9c7a960a68a6198b9bdf72f196e88ef6ca8f4
delboy1978uk <delboy1978uk@gmail.com>
2018-01-07 22:05:15 +0100

    test bootstrap

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -7,8 +7,8 @@ ini_set('xdebug.var_display_max_data', 1024);
 use Del\Common\ContainerService;
 use Del\Common\Config\DbCredentials;
 
-$array = require_once
-$creds = new DbCredentials();
+$array = require_once 'migrant-cfg.php';
+$creds = new DbCredentials($array['db']);
 
 return ContainerService::getInstance()
     ->setDbCredentials($creds)
