03f81bc97e82e630fbb5784b07f548310ad52122
Derek Stephen McLean <delboy1978uk@gmail.com>
2019-09-16 02:32:06 +0200

    use .migrant in test

--- tests/unit/_bootstrap.php
+++ tests/unit/_bootstrap.php
@@ -1,13 +1,9 @@
 <?php
 
-ini_set('xdebug.var_display_max_depth', 10);
-ini_set('xdebug.var_display_max_children', 256);
-ini_set('xdebug.var_display_max_data', 1024);
-
 use Del\Common\ContainerService;
 use Del\Common\Config\DbCredentials;
 
-$array = require_once 'migrant-cfg.php';
+$array = require_once '.migrant';
 $creds = new DbCredentials($array['db']);
 
 return ContainerService::getInstance()
